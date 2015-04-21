d63cfc946e49b09cb1f2c83b2ef33a9a12a93b1e
Stefano Mandelli <algebrato@lcm.mi.infn.it>
2015-04-21 22:03:02 +0200

    modified:   get_db.py

--- get_db.py
+++ get_db.py
@@ -4,14 +4,16 @@ import MySQLdb
 import getpass
 
 try:
-	server = raw_input('Server: ')
-	uid = raw_input('User: ')
+	#server = raw_input('Server: ')
+	server = "92.222.24.64"
+	#uid = raw_input('User: ')
+	uid ="root"
 	pwd = getpass.getpass()
 except:
 	print("ERRORRRRRR!!!!!!")
 
 try:
-	db = MySQLdb.connect(host=server, user=uid, passwd=pwd, db="mysql")
+	db = MySQLdb.connect(host=server, user=uid, passwd=pwd, db="Syslog")
 except:
 	print ""
 	print("Error connection database...")
@@ -22,10 +24,9 @@ try:
 	print "Fetching ..."
 	print ""
 	cur = db.cursor();
-	cur.execute("select * from user")
+	cur.execute("select * from SystemEvents")
 	for row in cur.fetchall() :
-		print row[0] + "\t" + row[1]
-	db.close()
+		print row[3],row[20],row[7]
 except:
 	print ""
 	print("Errore passo 2")
